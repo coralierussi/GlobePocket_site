e563cb01cb7f195e491e5c97d48bdb19aa26960c
coralie <russicoralie812@gmail.com>
2025-03-06 10:01:42 +0100

    Réinitialisation code

--- site/src/App.vue
+++ site/src/App.vue
@@ -1,28 +1,122 @@
 <template>
-  <div id="app">
+  <main id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <HeaderSite/>
+    <div class="presentation">
+        <div class="carousel">
+            <!-- <img class="carousel-images" src="./assets/SiteCSS/voyage-2.png" alt=""> -->
+            <div class="en-tete">
+                <p class="title">Suivez vos envies</p>
+            </div>
+        </div>
+    </div>
+    <div class="globe">
+        <div class="texte">
+
+        </div>
+        <div class="img">
+            <!-- <img class="img-globe" src="./assets/SiteCSS/earth.jpg" alt=""> -->
+        </div>
+    </div>
+  </main>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HeaderSite from './components/Header.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  // name: 'App',
+  components: { HeaderSite },
+  data(){
+    return {
+
+    }
+  },
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="sccs" scoped>
+*{
+    margin: 0%;
+    padding: 0%;
+
+    a{
+        text-decoration: none;
+    }
+    .header{
+        display: flex;
+        background-color: #7BB27F;
+        width: 100%;
+        height: 60px;
+        .logo{
+            width: 20%;
+            .logo-img{
+                border-radius: 100%;
+                background-color: grey;
+                width: 50px;
+                height: 50px;
+                margin: 5px;
+            }
+        }
+        .navbar{
+            display: flex;
+            margin-left: auto;
+            width: 60%;
+            .link-header{
+                color: black;
+                padding-right: 5px;
+                margin: 0%;
+            }
+        }
+        .compte{
+            width: 15%;
+            .compte-img{
+                border-radius: 100%;
+                background-color: rgb(34, 34, 204);
+                width: 50px;
+                height: 50px;
+                margin: 5px 10px;
+                float: right;
+            }
+        }
+    }
+    .presentation{
+        .carousel-images {
+            display: flex;
+            position: relative;
+            width: 200%;
+            animation: slide 4s infinite alternate ease-in-out;
+            z-index: 1;
+        }
+        .carousel img {
+            width: 100%;
+            height: auto;
+        }
+        /* @keyframes slide {
+            0% { transform: translateX(0); }
+            100% { transform: translateX(-50%); }
+        } */
+        .en-tete{
+            position: absolute;
+            width: 40%;
+            background-color: rgb(158, 158, 158);
+            border-radius: 50px;
+            /* margin-top: 20%;
+            margin-left: 30%; */
+            z-index: 10;
+            .title{
+                color: white;
+                font-size: 50px;
+                text-align: center;
+            }
+        }
+    }
+    .globe{
+        .img{
+            .img-globe{
+                width: 50%;
+            }
+        }
+    }
 }
 </style>
---
+++ site/src/components/Header.vue
@@ -0,0 +1,73 @@
+<template>
+  <main class="header d-flex">
+      <div class="logo">
+          <p class="logo-img" src="" alt=""/>
+      </div>
+      <div class="navbar d-flex">
+          <li><a class="link-header" href="/main.vue">Accueil</a></li>
+          <li><a class="link-header" href="/main.vue">Pays</a></li>
+          <li><a class="link-header" href="/main.vue">Activités</a></li>
+          <li><a class="link-header" href="/main.vue">Transports</a></li>
+          <li><a class="link-header" href="/main.vue">Hébergements</a></li>
+          <li><a class="link-header" href="/main.vue">Locations</a></li>
+      </div>
+      <div class="compte">
+          <!-- <img class="compte-img" src="/assests/SiteCSS/user.png" alt=""/> -->
+      </div>
+  </main>
+</template>
+
+<script>
+export default{
+        name: 'HeaderSite',
+        props:{
+
+        },
+        data (){
+            return {
+
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+ a{
+        text-decoration: none;
+    }
+    .header{
+        background-color: #7BB27F;
+        width: 100%;
+        height: 60px;
+        .logo{
+            width: 20%;
+            .logo-img{
+                border-radius: 100%;
+                background-color: grey;
+                width: 50px;
+                height: 50px;
+                margin: 5px;
+            }
+        }
+        .navbar{
+            margin-left: auto;
+            width: 60%;
+            .link-header{
+                color: black;
+                padding-right: 5px;
+                margin: 0%;
+            }
+        }
+        .compte{
+            width: 15%;
+            .compte-img{
+                border-radius: 100%;
+                background-color: rgb(34, 34, 204);
+                width: 50px;
+                height: 50px;
+                margin: 5px 10px;
+                float: right;
+            }
+        }
+    }
+</style>
