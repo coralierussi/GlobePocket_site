ed47f92b27fc9256dcee3cc59a804223033d0eb7
coralie <russicoralie812@gmail.com>
2025-03-11 21:21:58 +0100

    add component CoupCoeur

---
+++ site/src/components/CoupCoeurs.vue
@@ -0,0 +1,23 @@
+<template>
+    <main class="coup-coeurs d-flex">
+        
+    </main>
+  </template>
+  
+  <script>
+  export default{
+          name: 'CoupCoeurs',
+          props:{
+  
+          },
+          data (){
+              return {
+  
+              }
+          }
+      }
+  </script>
+  
+  <style lang="scss" scoped>
+  
+  </style>
