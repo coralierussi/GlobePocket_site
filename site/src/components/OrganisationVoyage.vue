8c5db43dc3b0623e5e8859955e7030069a85e54c
coralie <russicoralie812@gmail.com>
2025-03-17 16:39:52 +0100

    Add component OrganisationVoyage

---
+++ site/src/components/OrganisationVoyage.vue
@@ -0,0 +1,23 @@
+<template>
+    <v-navigation-drawer>
+        
+    </v-navigation-drawer>    
+</template>
+  
+  <script>
+  export default{
+          name: 'OrganisationVoyage',
+          props:{
+  
+          },
+          data (){
+              return {
+  
+              }
+          }
+      }
+  </script>
+  
+  <style lang="scss" scoped>
+  
+  </style>
