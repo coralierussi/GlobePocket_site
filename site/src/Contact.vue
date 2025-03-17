1bfbe8be0f4f613d9ca5f605f7fe335087e02a85
coralie <russicoralie812@gmail.com>
2025-03-17 14:16:35 +0100

    Add page Contact

---
+++ site/src/Contact.vue
@@ -0,0 +1,36 @@
+<template>
+    <main id="contact">
+      <HeaderSite/>
+      
+      <FooterSite/>
+    </main>
+  </template>
+  
+  <script>
+  import HeaderSite from './components/HeaderSite.vue';
+  import FooterSite from './components/FooterSite.vue';
+  
+  export default {
+    name: 'App',
+    components: { HeaderSite, FooterSite},
+    data(){
+      return {
+  
+      }
+    },
+  }
+  </script>
+  
+  <style sccs scoped>
+  
+  *{
+      margin: 0%;
+      padding: 0%;
+  
+  }
+  a{
+      text-decoration: none;
+  }
+  
+  </style>
+  
