50840ca7e5f5c0b118c4e4c521f38dfc2fe457e9
coralie <russicoralie812@gmail.com>
2025-05-18 12:31:57 +0200

    finish component

--- src/components/FooterSite.vue
+++ src/components/FooterSite.vue
@@ -7,39 +7,26 @@
                 </a>
             </div>
             <div class="footer-bar-globePocket">
-                <p class="titre-footer">Globe Pocket</p>
-                <li><a class="link-footer" href="/presentation">Qui sommes-nous ?</a> </li>
-                <li><a class="link-footer" href="/pays">Nous rejoindre</a></li>
-                <li><a class="link-footer" href="/activite">Conditions générales</a></li>
-            </div>
-            <div class="footer-bar-contact">
-                <p class="titre-footer">Contacts</p>
-                <li> <a class="link-footer" href="/presentation">Aide/FAQ</a></li>
-                <li> <a class="link-footer" href="/contact">Contactez-nous</a> </li>
-                <li><a class="link-footer" href="/paiementPage">Paiements sécurisés</a> </li>
-            </div>
-            <div class="footer-bar">
-                <li><a class="link-footer" href="/mentionLegales">Mentions légales</a></li>
-                <li><a class="link-footer" href="/politique">Politique cookies</a></li>
-                <li><a class="link-footer" href="/donneesPersonnel">Charte des données personnelles</a></li>
-            </div>
-            <div class="contact">
-                    <div class="rs d-flex">
-                        <a href="https://www.instagram.com/" target="_blank">
-                            <img class="img-rs" src="../assets/instagram.png" alt="Instagram">
-                        </a>
-                        <a href="https://www.facebook.com/" target="_blank">
-                            <img class="img-rs" src="../assets/facebook.png" alt="Facebook">
-                        </a>
-                        <a href="https://www.linkedin.com/feed/" target="_blank">
-                            <img class="img-rs" src="../assets/linkedin.png" alt="LinkedIn">
-                        </a>
-                        <a href="https://www.tiktok.com/fr/" target="_blank">
-                            <img class="img-rs" src="../assets/tik-tok.png" alt="TikTok">
-                        </a>
-                    </div>
-                    <p class="mail">globePocket@gmail.com</p>
-            </div>
+  <p class="titre-footer">Globe Pocket</p>
+  <li><router-link class="link-footer" to="/presentation">Qui sommes-nous ?</router-link></li>
+  <li><router-link class="link-footer" to="/pays">Nous rejoindre</router-link></li>
+  <li><router-link class="link-footer" to="/activites">Conditions générales</router-link></li>
+</div>
+
+<div class="footer-bar-contact">
+  <p class="titre-footer">Contacts</p>
+  <li><router-link class="link-footer" to="/presentation">Aide/FAQ</router-link></li>
+  <li><router-link class="link-footer" to="/contact">Contactez-nous</router-link></li>
+  <li><router-link class="link-footer" to="/paiementPage">Paiements sécurisés</router-link></li>
+</div>
+
+<div class="footer-bar">
+  <p class="titre-footer">Informations légales et confidentialité</p>
+  <li><router-link class="link-footer" to="/mentionLegales">Mentions légales</router-link></li>
+  <li><router-link class="link-footer" to="/politique">Politique cookies</router-link></li>
+  <li><router-link class="link-footer" to="/donneesPersonnel">Charte des données personnelles</router-link></li>
+</div>
+
         
     </div>
     <div class="legale">
@@ -70,6 +57,7 @@ a{
     width: 100%;
     overflow-y: hidden;
     margin-top: 60px;
+    background-color: rgba(244, 231, 198, 0.5);
     .footer{
     .logo{
       width: 15%;
@@ -81,7 +69,7 @@ a{
     }
     .footer-bar-globePocket, .footer-bar-contact, .footer-bar{
         margin-top: 10px;
-        width: 21%;
+        width: 28%;
         list-style: none;
         .titre-footer{
             color: #EAA854;
@@ -92,22 +80,7 @@ a{
           padding: 0rem 0.5rem;
         }
     }
-    .contact{
-      width: 20%;
-      margin-top: 10px;
-      .rs{
-          justify-content: center;
-          margin-bottom: 10px;
-          .img-rs{
-              width: 30px;
-              height: 30px;
-              margin: 5px 5px;
-          }
-      }
-      .mail{
-          text-align: center;
-      }
-    }
+    
     }
     .legale{
         border-top: solid 1px grey;
