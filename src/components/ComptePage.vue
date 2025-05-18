030c10bc96c172f4912f971466e1c0ba9575f678
coralie <russicoralie812@gmail.com>
2025-05-18 23:30:43 +0200

    create component

---
+++ src/components/ComptePage.vue
@@ -0,0 +1,23 @@
+<template>
+  <main id="comptePage">
+  </main>
+</template>
+
+<script>
+export default {
+  name: 'ComptePage',
+  data () {
+    return {
+
+    }
+      
+  },
+  methods: {
+    
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
