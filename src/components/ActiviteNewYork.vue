498a2ee5811a2f21918412725a3ffc228bd2abf2
coralie <russicoralie812@gmail.com>
2025-05-25 18:44:06 +0200

    Modif name composant

---
+++ src/components/ActiviteNewYork.vue
@@ -0,0 +1,25 @@
+<template>
+    <main id="activiteNY">
+        <HeaderSiteColor/>
+
+        <FooterSite/>
+    </main>
+</template>
+
+<script>
+import HeaderSiteColor from './HeaderSiteColor.vue';
+import FooterSite from './FooterSite.vue'
+
+export default {
+    name: 'ActiviteNY',
+    components: { HeaderSiteColor, FooterSite},
+    data(){
+      return {
+        
+      }
+    },
+  methods: {
+
+  }
+}
+</script>
